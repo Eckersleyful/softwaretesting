f11e9dd8b5db644580baf91fed0aa5300fe64c8a
Eckersleyful <ilmari.remora@gmail.com>
2020-11-26 15:08:25 +0200

    Fixed chmod access

--- testcases/basictest.js
+++ testcases/basictest.js
@@ -2,8 +2,9 @@ var assert = require('assert')
 
 describe('isEqual', function(){
     describe('#something', function(){
-        it("True if 1 = 1", function(){
+        it("True if 1 = 1", function(){   
             assert.strictEqual(1,1);
+            
         });
     });
 });
