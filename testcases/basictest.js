f1e1d130e9c6bdabd672f127c874d99f3593028c
Eckersleyful <ilmari.remora@gmail.com>
2020-11-26 15:02:49 +0200

    First test try

---
+++ testcases/basictest.js
@@ -0,0 +1,9 @@
+var assert = require('assert')
+
+describe('isEqual', function(){
+    describe('#something', function(){
+        it("True if 1 = 1", function(){
+            assert.strictEqual(1,1);
+        });
+    });
+});
