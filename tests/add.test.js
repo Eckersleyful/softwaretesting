13006e302ceb6d6b1f64f388b809b04b0e737582
Eckersleyful <ilmari.remora@gmail.com>
2020-12-03 17:22:19 +0200

    trying new wildcard coverage path

---
+++ tests/add.test.js
@@ -0,0 +1,22 @@
+import add from '../repototest/COMP.SE.200-2020-assignment/src/add.js';
+import assert from 'assert';
+import expect from 'chai';
+
+
+describe('add', function(){
+    describe('#adding two positive values', function(){
+        it("Try passing 1+1 = 2", function(){   
+           expect(add(1,1).to.eql(2));
+            
+        });
+    });
+});
+
+
+describe('add', function(){
+    describe('#adding two negative values', function(){
+        it("Try passing -1-1 = -2", function(){   
+            expect(add(-1,-1).to.eql(-2));
+        });
+    });
+});
