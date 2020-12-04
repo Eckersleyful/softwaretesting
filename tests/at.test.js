1ec7e93b92fa2fa739df1e8820e56925e7f97272
Eckersleyful <ilmari.remora@gmail.com>
2020-12-04 16:35:18 +0200

    at.test.js

--- tests/at.test.js
+++ tests/at.test.js
@@ -1,8 +1,7 @@
 import at from '../repototest/COMP.SE.200-2020-assignment/src/at.js';
 import pkg from "chai";
 const { expect, assert } = pkg;
-at(object, ['a[0].b.c', 'a[1]'])
- * // => [3, 4]
+
 
 describe('at', function(){
     describe('#Creates an array of values corresponding to `paths` of `object`.', function(){
