b4a32c45f9d03bacfd626e8572de4fc5563dbed8
Eckersleyful <ilmari.remora@gmail.com>
2020-12-07 17:40:40 +0200

    try done

--- tests/chunk.test.js
+++ tests/chunk.test.js
@@ -1,5 +1,7 @@
 import chunk from '../repototest/COMP.SE.200-2020-assignment/src/chunk.js';
 import pkg from "chai";
+import mocha from "mocha";
+const {done} = mocha
 const { expect, assert } = pkg;
 
 
@@ -7,14 +9,20 @@ describe('chunk', function(){
     describe(`#Creates an array of elements split into groups the length of size
     If array cant be split evenly, the final chunk will be the remaining
     elements`, function(){
-        it.skip("Try creating chunks that split evenly", function(){   
-                
-            expect(chunk(['a', 'b', 'c', 'd'], 2)).to.deep.eql([['a', 'b'], ['c', 'd']]);
+        it("Try creating chunks that split evenly", function(done){   
+            try{
+                expect(chunk(['a', 'b', 'c', 'd'], 2)).to.deep.eql([['a', 'b'], ['c', 'd']]);
+                done()
+            }
+            catch(e){
+                done(e)
+            }
 
         });
         it.skip("Try creating chunks that split unevenly", function(){   
                 
             expect(chunk(['a', 'b', 'c', 'd'], 3)).to.deep.eql([['a', 'b', 'c'], ['d']]);
+            
 
         });
         it.skip("Try creating chunk of equal size to the array", function(){   
